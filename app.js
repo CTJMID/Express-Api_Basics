39f6c0e33844b6aeaaa751ec4c341e4fd354c5d3
Chris Middleton <ctjmiddleton@gmail.com>
2022-05-20 17:27:46 +0100

    remove gitignore files

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const res = require('express/lib/response');
-const app = express();
-
-app.get('/', (request, response) => {
-    res.send('Hello World')
-});
-
-app.listen(3000, () => console.log('Listening on port 3000'))
