93acb04d0969e99f494d91bf864786efdc8a822b
Chris Middleton <ctjmiddleton@gmail.com>
2022-05-23 11:58:50 +0100

    debugging for routes to pass in postman

--- index.js
+++ index.js
@@ -49,9 +49,9 @@ app.put('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) res.status(404).send('The course with the given id was not found');
 
-    const { error } = validateCourse(req.body)
-    if (error) {
-        res.status(400).send(result.error.details[0].message);
+    const { error } = validateCourse(req.body);
+    if(error) {
+        res.status(400).send(error.details[0].message);
         return;
     }
 
@@ -64,6 +64,7 @@ function validateCourse(course) {
     const schema = Joi.object({
         name: Joi.string().min(4).required()
     });
+   
     return schema.validate(course);
 }
 
